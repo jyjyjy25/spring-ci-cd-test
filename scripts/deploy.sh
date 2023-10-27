1a59966b2a50b5a4e5be67b927a14d69eb7794cc
jyjyjy25 <jyjyjy7418@gmail.com>
2023-10-28 00:27:39 +0900

    [ADD] 쉘 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,31 +1,29 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-REPOSITORY=/home/ubuntu/spring-github-action
+ROOT_PATH="/home/ubuntu/spring-github-action"
+JAR_NAME=$(ls ROOT_PATH/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_PATH=ROOT_PATH/build/libs/$JAR_NAME
+STOP_LOG="$ROOT_PATH/stop.log"
+SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
 
-echo "> 현재 구동 중인 애플리케이션 pid 확인"
-
-CURRENT_PID=$(pgrep -fla java | grep hayan | awk '{print $1}')
-
-echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
-
-if [ -z "$CURRENT_PID" ]; then
-  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+if [ -z "$SERVICE_PID" ]; then
+  echo "서비스 NouFound" >> $STOP_LOG
 else
-  echo "> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 5
-fi
+  echo "서비스 종료 " >> $STOP_LOG
+  kill "$SERVICE_PID"
+  # kill -9 $SERVICE_PID # 강제 종료를 하고 싶다면 이 명령어 사용
 
-echo "> 새 애플리케이션 배포"
+APP_LOG="$ROOT_PATH/application.log"
+ERROR_LOG="$ROOT_PATH/error.log"
+START_LOG="$ROOT_PATH/start.log"
 
-JAR_NAME=$(ls -tr $REPOSITORY/build/libs/*SNAPSHOT.jar | tail -n 1)
+NOW=$(date +%c)
 
-echo "> JAR NAME: $JAR_NAME"
+echo "[$NOW] $JAR 복사" >> $START_LOG
+cp $JAR_PATH $ROOT_PATH/$JAR_NAME
 
-echo "> $JAR_NAME 에 실행 권한 추가"
+echo "[$NOW] > $JAR 실행" >> $START_LOG
+nohup java -jar $ROOT_PATH/$JAR_NAME > $APP_LOG 2> $ERROR_LOG &
 
-chmod +x $JAR_NAME
-
-echo "> $JAR_NAME 실행"
-
-nohup java -jar -Duser.timezone=Asia/Seoul $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
+SERVICE_PID=$(pgrep -f $ROOT_PATH/$JAR_NAME)
+echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
